4791bd9f8d4410e6388f68af0726723ca3db358f
Divyam1201 <divyam2001dec@gmail.com>
2023-12-24 01:03:34 +0530

    project added

---
+++ README.md
@@ -0,0 +1,48 @@
+# Password Generator
+
+This project is a simple password generator built with React and Vite. It allows users to generate a random password based on their preferences.
+
+## Features
+
+- Generate a random password with a length between 1 and 100 characters.
+- Option to include numbers in the password.
+- Option to include special characters in the password.
+- Copy the generated password to the clipboard.
+
+## How to Use
+
+1. Adjust the length of the password using the slider.
+2. Check the "Numbers" box if you want to include numbers in your password.
+3. Check the "Special Characters" box if you want to include special characters in your password.
+4. Click the "Generate" button to generate a new password.
+5. Click the "Copy" button to copy the generated password to your clipboard.
+
+## Installation
+
+To install and run this project locally, follow these steps:
+
+1. Clone the repository:
+  ```bash
+     git clone https://github.com/yourusername/password-generator.git
+  ```
+
+2. Install the dependencies:
+  ```bash
+     cd password-generator
+     npm install
+  ```
+
+3. Start the application:
+  ```bash
+     npm run dev
+  ```
+
+The application will start on localhost:5000.
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+
+This project is licensed under the MIT License.
---
+++ src/App.jsx
@@ -0,0 +1,88 @@
+import { useCallback, useRef, useState } from "react";
+import bg from "./assets/bg.jpg";
+
+function App() {
+  const [length, setLength] = useState(5);
+  const [isNumberAllowed,setIsNumberAllowed]=useState(false)
+  const [isCharAllowed,setIsCharAllowed]=useState(false)
+  const [password, setPassword] = useState("");
+let passwordRef=useRef(null)
+const generatePassword=useCallback(()=>{
+  let pass=""
+  let letters="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+  for (let i = 1; i <= length; i++) {
+    if (isNumberAllowed) {
+      letters+="1234567890"
+    }
+    if (isCharAllowed) {
+      letters+="~!:<>,.'?/\|``@#$%^&*()]"
+    }
+    let char=Math.floor(Math.random()*letters.length)
+    pass+=letters.charAt(char)
+    setPassword(pass)
+  }
+
+},[length,isCharAllowed,isNumberAllowed,setPassword])
+let copyPassword=()=>{
+  if (password) {
+document.getElementById("copybanner").classList.remove("hidden")
+passwordRef.current?.select()
+window.navigator.clipboard.writeText(password)
+setTimeout((()=>{
+  document.getElementById("copybanner").classList.add("hidden")
+  passwordRef.current.setSelectionRange(0,0)
+  passwordRef.current.blur()
+}),2000)
+}
+else{
+  alert("First generate a password then copy it")
+}
+}
+  return (
+    <>
+      <div className="w-full h-screen  text-gray-950 overflow-hidden bg-neutral-800">
+        <div id="copybanner" className="px-3 absolute top-[7%] md:top-[10%] left-[50%] -translate-x-1/2 -translate-y-1/2  text-white text-xs md:text-sm  bg-amber-500 shadow-[3px_3px_10px] shadow-amber-600 hidden rounded-xl">Password copied to clipboard</div>
+      <div className="px-1 absolute top-[20%] md:top-[30%] left-[50%] -translate-x-1/2 -translate-y-1/2  text-white text-5xl w-full md:w-max text-center">
+          Password Generator
+        </div>
+        <div className="p-5 rounded-2xl flex flex-col absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 border-gray-800 bg-amber-500 shadow-[5px_5px_15px] shadow-amber-600">
+          <div>
+            <input
+              type="text"
+              className="rounded-xl ps-3"
+              value={password}
+              ref={passwordRef}
+              readOnly
+            />
+            <button className=" px-3  border-2 border-gray-700 rounded-xl text-white bg-green-600 hover:grayscale-[70%] transition-all ease-in duration-300 mx-[35%] my-2 md:m-1" onClick={copyPassword}>
+              Copy
+            </button>
+          </div>
+          <div>
+            <input type="range" onChange={(e)=>{
+                setLength(e.target.value)
+              }} min={1} max={100} value={length} />
+            <label>length({length})</label>
+          </div>
+          <label>
+            <input type="checkbox" name="" id="" className="m-1" onChange={(e)=>{
+                setIsCharAllowed(e.target.checked)
+              }} />
+            Special Characters
+          </label>
+          <label>
+            <input type="checkbox" name="" id="" className="m-1" onChange={(e)=>{
+                setIsNumberAllowed(e.target.checked)
+              }} />
+            Numbers
+          </label>
+        </div>
+        <button className="px-3 rounded-2xl absolute top-[70%] left-[50%] -translate-x-1/2 -translate-y-1/2  text-white bg-green-700 hover:grayscale-[70%]" onClick={generatePassword}>
+          Generate
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
